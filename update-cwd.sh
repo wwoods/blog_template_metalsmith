3a77ab10fdc696db57424a31fc46bce32efaca58
Walt Woods <woodswalben@gmail.com>
2017-11-04 16:38:16 -0700

    Updating again

--- update-cwd.sh
+++ update-cwd.sh
@@ -14,13 +14,13 @@ EXISTS=$(stat ./index.ts >/dev/null 2>&1 && echo 1 || echo 0)
 if [ "${EXISTS}" -eq "1" ]; then
     echo "Updating core site files from TEMPLATE=${TEMPLATE}"
     cp --preserve=timestamps -r ${TEMPLATE}/.gitignore ${TEMPLATE}/index.ts ${TEMPLATE}/layouts ${TEMPLATE}/package.json ${TEMPLATE}/plugin ${TEMPLATE}/tsconfig.json ./
-    cp --preserve=timestamps ${TEMPLATE}/content/styleDefault.scss ./content/styleDefault.scss
+    cp --preserve=timestamps ${TEMPLATE}/contents/styleDefault.scss ./contents/styleDefault.scss
     npm install
 else
     echo "Making NEW SITE from TEMPLATE=${TEMPLATE}"
     cp --preserve=timestamps -r ${TEMPLATE}/*.ts ${TEMPLATE}/package.json ${TEMPLATE}/layouts ${TEMPLATE}/plugin ${TEMPLATE}/tsconfig.json ${TEMPLATE}/.gitignore ./
-    mkdir ./content
-    cp --preserve=timestamps -r ${TEMPLATE}/content/styleDefault.scss ${TEMPLATE}/content/reset.css ./content
+    mkdir ./contents
+    cp --preserve=timestamps -r ${TEMPLATE}/contents/styleDefault.scss ${TEMPLATE}/contents/reset.css ./contents
     npm install
 fi
 
